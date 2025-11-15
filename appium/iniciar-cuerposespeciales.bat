7daf1b5bb64565c013ce5a03f51ec82a0bdc3cdc
jarconett <67648532+jarconett@users.noreply.github.com>
2025-11-15 16:31:20 +0100

    Update iniciar-cuerposespeciales.bat

--- appium/iniciar-cuerposespeciales.bat
+++ appium/iniciar-cuerposespeciales.bat
@@ -1,5 +1,6 @@
 @echo off
 cd /d "C:\Users\javir\Desktop\CESP_APPIUM"
-powershell.exe -ExecutionPolicy Bypass -File "start-all-cuerposespeciales.ps1"
+powershell.exe -ExecutionPolicy Bypass -File "start-all-c_especiales.ps1"
 pause
+
 
