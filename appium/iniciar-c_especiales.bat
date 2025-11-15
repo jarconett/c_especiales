eb518e26d8e230d8eb33180057c87a36adeb9fc6
jarconett <67648532+jarconett@users.noreply.github.com>
2025-11-15 16:42:20 +0100

    Add files via upload

---
+++ appium/iniciar-c_especiales.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d "C:\Users\javir\Desktop\CESP_APPIUM"
+powershell.exe -ExecutionPolicy Bypass -File "start-all-c_especiales.ps1"
+pause
+
---
+++ appium/iniciar-test.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d "C:\Users\javir\Desktop\CESP_APPIUM"
+powershell.exe -ExecutionPolicy Bypass -File "start-all.ps1"
+pause
+
